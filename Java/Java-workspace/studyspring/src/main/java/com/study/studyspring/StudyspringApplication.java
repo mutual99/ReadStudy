0693eb8d55a211dc4a22e37160cf7d24ba279411
mutual99 <152463166+mutual99@users.noreply.github.com>
2024-04-24 11:43:37 +0900

    스프링부트 시작

---
+++ Java/Java-workspace/studyspring/src/main/java/com/study/studyspring/StudyspringApplication.java
@@ -0,0 +1,13 @@
+package com.study.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyspringApplication.class, args);
+	}
+
+}
